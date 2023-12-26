3da90df266a38dd5559395565ea431d62838e51e
Atulin <11233299+Atulin@users.noreply.github.com>
2023-12-26 02:44:28 +0100

    Mostly done

---
+++ RedditStoryDl/Helpers/DirHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace RedditStoryDl.Helpers;
+
+public class DirHelpers
+{
+    public static string GetFilePath(string directory, string title)
+    {
+        var filename = $"{title}-{Random.Shared.Next()}.md";
+        return FullPath(directory, filename);
+    }
+
+    [Pure]
+    public static string FullPath(params string[] segments) => Path.GetFullPath(Path.Join(segments));
+
+}
