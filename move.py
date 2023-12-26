6aa55c12ee8a3a16d700ecf323489711891cdad5
Atulin <11233299+Atulin@users.noreply.github.com>
2023-12-26 04:39:30 +0100

    The scuffed build system is ready lmao

---
+++ move.py
@@ -0,0 +1,7 @@
+﻿import glob
+import subprocess
+
+files = glob.glob('./publish/*/*')
+for f in files:
+    rid = f.replace('\\', '/').split('/')[-2]
+    subprocess.run(f"7z a ./publish/{rid}.zip {f}")
